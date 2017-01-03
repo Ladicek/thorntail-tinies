5ee4a447e42d0c046331e5e0545a4cb02b2f85eb
Ladislav Thon <lthon@redhat.com>
2017-01-03 13:17:13 +0100

    add an experiment with forced fraction autodetection and explicitly added EJB fraction

---
+++ fraction-autodetection/explicit-ejb-fraction/src/main/java/cz/ladicek/swarm/tiny/fractionAutodetectionExplicitEjbFraction/HelloBean.java
@@ -0,0 +1,10 @@
+package cz.ladicek.swarm.tiny.fractionAutodetectionExplicitEjbFraction;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class HelloBean {
+    public String hello() {
+        return "Hello from EJB";
+    }
+}
