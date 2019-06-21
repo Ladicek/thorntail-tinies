67e8d1699b71bfa847a35be34b7a5598190df039
Ladislav Thon <lthon@redhat.com>
2019-06-21 14:50:50 +0200

    add an experiment with Deployment Scanner (to perform hot redeployment)

---
+++ hot-deployment/src/main/java/cz/ladicek/swarm/tiny/hotDeployment/HelloServlet.java
@@ -0,0 +1,15 @@
+package cz.ladicek.swarm.tiny.hotDeployment;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Hello from servlet");
+    }
+}
